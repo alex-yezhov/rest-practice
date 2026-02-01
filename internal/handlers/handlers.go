fa968b67b6e0c30cc004034d71d27a3c31609260
OleksandrY <oy@mellifera.team>
2026-02-01 18:45:03 +0100

    Done?

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,40 @@
 package handlers
+
+import (
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	file, header, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	result, err := service.Reader(file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	ext := filepath.Ext(header.Filename)
+	name := time.Now().UTC().String() + ext
+
+	if err := os.WriteFile(name, []byte(result), 0o644); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte(result))
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,36 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+type Server struct {
+	Logger *log.Logger
+	HTTP   *http.Server
+}
+
+func New(logger *log.Logger) *Server {
+	mux := http.NewServeMux()
+
+	// Роуты
+	mux.HandleFunc("/", handlers.MainHandler)
+	mux.HandleFunc("/upload", handlers.UploadHandler)
+
+	httpServer := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	return &Server{
+		Logger: logger,
+		HTTP:   httpServer,
+	}
+}
